8d6f2f812c18f5862fa6585ab5c1e6fa98a47487
rickytang <rickytang11111@gmail.com>
2025-04-12 21:56:53 +0800

    update

---
+++ demo-datatype /src/DemoDataType.java
@@ -0,0 +1,63 @@
+public class DemoDataType {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //!Integer(+ve - -ve)
+        //"=" -> assigement
+        int x = 3;
+        // put 7 into y
+        int y = 7;
+
+        System.out.println("x");
+        System.out.println(x);
+        System.out.println("y");
+        //Reassign value 10 into variable x 
+        x = 10;
+        System.out.println(x);//10
+
+        y = 100;
+        System.out.println(y);
+
+        int salary = 20000;
+        // Variable name conversation 
+        int tutorName = 5;
+        System.out.println(tutorName);
+
+        // Declation of variable type 聲明變量類型 
+        int a;
+        // same varible name can be declared once only 
+        // assignemnt 
+        a = 4;
+        System.out.println(a);
+        int w = 10;
+        w = 9;
+        System.out.println(w);
+
+        // ! double 
+        double d1 = 4.4;
+        System.out.println(d1);
+
+        double d2 = 7.364;
+        System.out.println(d2);
+
+        double d3 = 5.5;
+        System.out.println(d3);
+
+        int x2 = 6 + 1;
+        System.out.println(x2);
+
+        int x3 = 9;
+        x2 = x3;
+        System.out.println(x2);
+
+        int x4 = 10;
+        x2 = 7 + x4;
+        System.out.println(x2);
+
+        x2 = x2 + 8;
+        System.out.println(x2);
+        
+
+
+    }
+}
